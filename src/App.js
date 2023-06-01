cf83b337abcf1fa9bc3070d9c5ab515d02cfe8a2
Abi0l9 <cryptog3ms@gmail.com>
2023-06-01 08:35:41 +0100

    installed axios for fetching data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import Layout from "./components/Layout";
 
 function App() {
+  console.log("key iss :" + process.env.REACT_APP_API_KEY);
   return (
     <Box sx={{}}>
       <Layout />
--- src/components/Footer/FooterContent.jsx
+++ src/components/Footer/FooterContent.jsx
@@ -1,24 +1,21 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Divider, Menu, MenuItem, Stack, Typography } from "@mui/material";
 import React from "react";
 import { HorizontalStack } from "../../styled";
 import { GMobiledata } from "@mui/icons-material";
 
 function FooterContent() {
   return (
-    <Box sx={{ padding: "24px" }}>
+    <Box sx={{ padding: "24px", pb: 0 }}>
       <HorizontalStack>
         <Box width="50%">
-          <Stack justifyContent="space-between">
-            <Stack spacing={3}>
-              <GMobiledata />
-              <Typography>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit
-                vel nam impedit, voluptatibus sunt dicta eius reiciendis
-                laudantium earum ipsam perspiciatis. In qui hic quaerat labore
-                harum dolore dolores assumenda!
-              </Typography>
-            </Stack>
-            <Box sx={{ mt: "80px" }}>Copyright News 24hrs</Box>
+          <Stack spacing={3}>
+            <GMobiledata />
+            <Typography>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit vel
+              nam impedit, voluptatibus sunt dicta eius reiciendis laudantium
+              earum ipsam perspiciatis. In qui hic quaerat labore harum dolore
+              dolores assumenda!
+            </Typography>
           </Stack>
         </Box>
         <Box>
@@ -50,6 +47,14 @@ function FooterContent() {
             <Typography>Facebook Twitter</Typography>
           </Stack>
         </Box>
+      </HorizontalStack>
+      <Divider />
+      <HorizontalStack>
+        <Box sx={{ my: 0 }}>Copyright News 24hrs</Box>
+        {/* <Menu>
+          <MenuItem>EN</MenuItem>
+          <MenuItem>AR</MenuItem>
+        </Menu> */}
       </HorizontalStack>
     </Box>
   );
